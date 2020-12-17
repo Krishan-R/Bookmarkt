877c3ada4e51c8c88398d941b84d606dfe77e0cb
krishan <krishan.raithatha274@gmail.com>
2020-12-17 15:27:27 +0000

    able to scrape books with given google ID

--- Bookmarkt Flask/book.py
+++ Bookmarkt Flask/book.py
@@ -2,33 +2,65 @@ import requests
 
 
 class Book:
-    def __init__(self, isbn):
+    def __init__(self, isbn="", googleID=""):
         self.isbn = isbn
+        self.googleID = googleID
 
         self.title = ""
         self.author = ""
         self.description = ""
-        self.googleID = ""
 
-        self.__scrapeBookDataISBN()
+        if self.isbn != "":
+            self.__scrapeBookDataISBN()
+        elif self.googleID != "":
+            self.__scrapeBookDataGoogleID()
+        else:
+            print("no isbn or google books ID provided, automatic scraping will not occur")
 
     def __scrapeBookDataISBN(self):
 
-        apiKey = "AIzaSyBu5i0kpWKfoJ0Juhg5lhpYCU5Xonodo8g"
-        orderBy = "relevance"
+        if self.isbn is not None or self.isbn != "":
 
-        r = requests.get(
-            f"https://www.googleapis.com/books/v1/volumes?q=isbn:{self.isbn}&orderBy={orderBy}&key={apiKey}")
+            apiKey = "AIzaSyBu5i0kpWKfoJ0Juhg5lhpYCU5Xonodo8g"
+            orderBy = "relevance"
 
-        parsedJson = r.json()
+            r = requests.get(
+                f"https://www.googleapis.com/books/v1/volumes?q=isbn:{self.isbn}&orderBy={orderBy}&key={apiKey}")
 
-        if parsedJson["totalItems"] > 0:
-            self.title = parsedJson["items"][0]["volumeInfo"]["title"]
-            self.author = parsedJson["items"][0]["volumeInfo"]["authors"]
-            self.description = parsedJson["items"][0]["volumeInfo"]["description"]
-            self.googleID = parsedJson["items"][0]["id"]
+            parsedJson = r.json()
+
+            if parsedJson["totalItems"] > 0:
+                self.title = parsedJson["items"][0]["volumeInfo"]["title"]
+                self.author = parsedJson["items"][0]["volumeInfo"]["authors"]
+                self.description = parsedJson["items"][0]["volumeInfo"]["description"]
+                self.googleID = parsedJson["items"][0]["id"]
+            else:
+                print(f"book not found with isbn: {self.isbn}")
         else:
-            print(f"book not found with isbn: {self.isbn}")
+            print("isbn empty")
+
+    def __scrapeBookDataGoogleID(self):
+
+        if self.googleID is not None or self.googleID != "":
+
+            apiKey = "AIzaSyBu5i0kpWKfoJ0Juhg5lhpYCU5Xonodo8g"
+
+            r = requests.get(
+                f"https://www.googleapis.com/books/v1/volumes/{self.googleID}&key={apiKey}")
+
+            parsedJson = r.json()
+
+            try:
+                self.title = parsedJson["volumeInfo"]["title"]
+                self.author = parsedJson["volumeInfo"]["authors"]
+                self.description = parsedJson["volumeInfo"]["description"]
+                self.isbn = parsedJson["volumeInfo"]["industryIdentifiers"][1]["identifier"]
+            except KeyError:
+                print(f"book not found with google ID: {self.googleID}")
+
+        else:
+            print("google ID empty")
+
 
     def getData(self):
         return [
