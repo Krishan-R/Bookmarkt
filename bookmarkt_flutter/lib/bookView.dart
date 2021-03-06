09ccd6129d4e9351ece7040936a56e841f7fad0e
krishan <krishan.raithatha274@gmail.com>
2021-03-06 16:46:24 +0000

    added pages read graph

--- bookmarkt_flutter/lib/bookView.dart
+++ bookmarkt_flutter/lib/bookView.dart
@@ -62,7 +62,7 @@ class _bookViewState extends State<bookView> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
+        child: ListView(
           children: [
             bookHeader(args),
             SizedBox(
@@ -71,33 +71,35 @@ class _bookViewState extends State<bookView> {
             InkWell(
               onTap: () {
                 showDialog(
-                    context: context,
-                    builder: (context) {
-                      return StatefulBuilder(builder: (context, setState) {
-                        return AlertDialog(
-                          content: Expanded(
-                            child: SingleChildScrollView(
-                              child: Text(
-                                args.book.description,
-                                style: TextStyle(fontSize: 15),
-                              ),
+                  context: context,
+                  builder: (context) {
+                    return StatefulBuilder(builder: (context, setState) {
+                      return AlertDialog(
+                        content: Expanded(
+                          child: SingleChildScrollView(
+                            child: Text(
+                              args.book.description,
+                              style: TextStyle(fontSize: 15),
                             ),
                           ),
-                          actions: [
-                            FlatButton(
-                              child: Text("OK"),
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                            )
-                          ],
-                        );
-                      });
+                        ),
+                        actions: [
+                          FlatButton(
+                            child: Text("OK"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          )
+                        ],
+                      );
                     });
+                  },
+                );
               },
               child: Text(
                 args.book.description,
                 maxLines: 3,
+                textAlign: TextAlign.center,
               ),
             ),
             Container(
@@ -184,44 +186,82 @@ class _bookViewState extends State<bookView> {
                 ),
               ],
             ),
-            Text("Time Spent Reading - 30 Days"),
+
             FutureBuilder(
-              future: getReadingStatistics(args),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-
-                  if (snapshot.data["maxY"] == 0) {
-                    return Text("Please read to show history");
-                  }
-
-                  return Stack(
-                    children: <Widget>[
-                      AspectRatio(
-                        aspectRatio: 1.70,
-                        child: Container(
-                          decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(18),
-                              ),
-                              color: Color(0xff232d37)),
-                          child: Padding(
-                            padding: const EdgeInsets.only(
-                                right: 18.0, left: 12.0, top: 24, bottom: 12),
-                            child: LineChart(
-                              mainData(snapshot.data),
-                            ),
-                          ),
+                future: getReadingStatistics(args),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data["timeData"]["maxY"] == 0) {
+                      return Text("Please read to show history");
+                    }
+                    return Column(
+                      children: [
+                        Text(
+                          "Time Spent Reading - 30 Days",
+                          textAlign: TextAlign.center,
                         ),
-                      ),
-                    ],
-                  );
-                } else if (snapshot.hasError) {
-                  return Text("${snapshot.error}");
-                }
-                return CircularProgressIndicator();
-              }
-
-            ),
+                        Stack(
+                          children: <Widget>[
+                            AspectRatio(
+                              aspectRatio: 1.70,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(18),
+                                    ),
+                                    color: Color(0xff232d37)),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 18.0,
+                                      left: 12.0,
+                                      top: 24,
+                                      bottom: 12),
+                                  child: LineChart(
+                                    timeReadData(snapshot.data),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Pages Read - 30 Days",
+                          textAlign: TextAlign.center,
+                        ),
+                        Stack(
+                          children: <Widget>[
+                            AspectRatio(
+                              aspectRatio: 1.70,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(18),
+                                    ),
+                                    color: Color(0xff232d37)),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 18.0,
+                                      left: 12.0,
+                                      top: 24,
+                                      bottom: 12),
+                                  child: LineChart(
+                                    pagesReadData(snapshot.data),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+                  return CircularProgressIndicator();
+                }),
           ],
         ),
       ),
@@ -438,7 +478,7 @@ addReadingSessionAlert(BuildContext context, NavigatorArguments args) {
       });
 }
 
-LineChartData mainData(data) {
+LineChartData timeReadData(data) {
   List<Color> gradientColors = [
     const Color(0xff23b6e6),
     const Color(0xff02d39a),
@@ -475,9 +515,10 @@ LineChartData mainData(data) {
             fontWeight: FontWeight.bold,
             fontSize: 14),
         getTitles: (value) {
-
-          if (value % 7 == 0) return data["dateData"][value].substring(5, data["dateData"][value].length).replaceAll("-", "/");
-
+          if (value % 7 == 0)
+            return data["dateData"][value]
+                .substring(5, data["dateData"][value].length)
+                .replaceAll("-", "/");
           return '';
         },
         margin: 8,
@@ -490,14 +531,13 @@ LineChartData mainData(data) {
           fontSize: 15,
         ),
         getTitles: (value) {
-
-          if (data["maxY"] >= 200) {
+          if (data["timeData"]["maxY"] >= 200) {
             if (value % 50 == 0) return value.toInt().toString();
-          } else if (data["maxY"] >= 100) {
+          } else if (data["timeData"]["maxY"] >= 100) {
             if (value % 25 == 0) return value.toInt().toString();
-          } else if (data["maxY"] >= 50) {
+          } else if (data["timeData"]["maxY"] >= 50) {
             if (value % 25 == 0) return value.toInt().toString();
-          } else if (data["maxY"] >= 10) {
+          } else if (data["timeData"]["maxY"] >= 10) {
             if (value % 10 == 0) return value.toInt().toString();
           }
 
@@ -511,12 +551,109 @@ LineChartData mainData(data) {
         show: true,
         border: Border.all(color: const Color(0xff37434d), width: 1)),
     minX: 0,
-    maxX: data["maxX"],
+    maxX: data["timeData"]["maxX"],
     minY: 0,
-    maxY: data["maxY"],
+    maxY: data["timeData"]["maxY"],
     lineBarsData: [
       LineChartBarData(
-        spots: data["timeData"],
+        spots: data["timeData"]["data"],
+        isCurved: false,
+        colors: gradientColors,
+        barWidth: 2,
+        isStrokeCapRound: true,
+        dotData: FlDotData(
+          show: false,
+        ),
+        belowBarData: BarAreaData(
+          show: true,
+          colors:
+              gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+        ),
+      ),
+    ],
+  );
+}
+
+LineChartData pagesReadData(data) {
+  List<Color> gradientColors = [
+    const Color(0xff23b6e6),
+    const Color(0xff02d39a),
+  ];
+
+  return LineChartData(
+    lineTouchData: LineTouchData(
+      enabled: true,
+    ),
+    gridData: FlGridData(
+      show: true,
+      drawVerticalLine: true,
+      getDrawingHorizontalLine: (value) {
+        return FlLine(
+          color: const Color(0xff37434d),
+          strokeWidth: 1,
+        );
+      },
+      getDrawingVerticalLine: (value) {
+        return FlLine(
+          color: const Color(0xff37434d),
+          strokeWidth: 1,
+        );
+      },
+    ),
+    titlesData: FlTitlesData(
+      show: true,
+      bottomTitles: SideTitles(
+        rotateAngle: 0,
+        showTitles: true,
+        reservedSize: 22,
+        getTextStyles: (value) => const TextStyle(
+            color: Color(0xff68737d),
+            fontWeight: FontWeight.bold,
+            fontSize: 14),
+        getTitles: (value) {
+          if (value % 7 == 0)
+            return data["dateData"][value]
+                .substring(5, data["dateData"][value].length)
+                .replaceAll("-", "/");
+
+          return '';
+        },
+        margin: 8,
+      ),
+      leftTitles: SideTitles(
+        showTitles: true,
+        getTextStyles: (value) => const TextStyle(
+          color: Color(0xff67727d),
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
+        getTitles: (value) {
+          if (data["pageData"]["maxY"] >= 200) {
+            if (value % 50 == 0) return value.toInt().toString();
+          } else if (data["pageData"]["maxY"] >= 100) {
+            if (value % 25 == 0) return value.toInt().toString();
+          } else if (data["pageData"]["maxY"] >= 50) {
+            if (value % 25 == 0) return value.toInt().toString();
+          } else if (data["pageData"]["maxY"] >= 10) {
+            if (value % 10 == 0) return value.toInt().toString();
+          }
+
+          return '';
+        },
+        reservedSize: 28,
+        margin: 12,
+      ),
+    ),
+    borderData: FlBorderData(
+        show: true,
+        border: Border.all(color: const Color(0xff37434d), width: 1)),
+    minX: 0,
+    maxX: data["pageData"]["maxX"],
+    minY: 0,
+    maxY: data["pageData"]["maxY"],
+    lineBarsData: [
+      LineChartBarData(
+        spots: data["pageData"]["data"],
         isCurved: false,
         colors: gradientColors,
         barWidth: 2,
@@ -535,20 +672,19 @@ LineChartData mainData(data) {
 }
 
 Future<Map> getReadingStatistics(NavigatorArguments args) async {
-
   List<FlSpot> timeList = [];
+  List<FlSpot> pageList = [];
   Map dateData = {};
 
   try {
+    final response = await http.get(
+        "http://${args.url}:5000/users/${args.user.userID}/books/${args.book.bookInstanceID}/stats?time=10");
 
-    final response = await http.get("http://${args.url}:5000/users/${args.user.userID}/books/${args.book.bookInstanceID}/stats?time=30");
-
-    Iterable i = json.decode(response.body)["statistics"]["time"];
+    Iterable time = json.decode(response.body)["statistics"]["time"];
 
     double maxY = 0;
     double xValue = 0;
-    for (var val in i) {
-
+    for (var val in time) {
       timeList.add(FlSpot(xValue, val["time"].toDouble()));
       dateData[xValue] = val["date"];
 
@@ -556,12 +692,39 @@ Future<Map> getReadingStatistics(NavigatorArguments args) async {
       xValue += 1;
     }
 
-    Map returnData = {"maxX": xValue.toDouble()-1, "maxY": maxY, "timeData": timeList, "dateData": dateData};
+    Map timeData = {
+      "maxX": xValue.toDouble() - 1,
+      "maxY": maxY,
+      "data": timeList
+    };
+
+    Iterable pages = json.decode(response.body)["statistics"]["pages"];
+
+    maxY = 0;
+    xValue = 0;
+    for (var val in pages) {
+      pageList.add(FlSpot(xValue, val["pages"].toDouble()));
+
+      if (val["pages"].toDouble() > maxY) maxY = val["pages"].toDouble();
+      xValue += 1;
+    }
+
+    Map pageData = {
+      "maxX": xValue.toDouble() - 1,
+      "maxY": maxY,
+      "data": pageList
+    };
+
+    Map returnData = {
+      "timeData": timeData,
+      "dateData": dateData,
+      "pageData": pageData
+    };
+
+    print(returnData);
 
     return returnData;
-
   } on SocketException {
     print("error connecting to server");
   }
-
 }
