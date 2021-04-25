a3b3473049fc83f1b34638a345244416428b64de
krishan <krishan.raithatha274@gmail.com>
2021-04-25 12:13:26 +0100

    redesigned register page

--- bookmarkt_flutter/lib/Pages/signUp.dart
+++ bookmarkt_flutter/lib/Pages/signUp.dart
@@ -3,7 +3,6 @@ import 'package:bookmarkt_flutter/Models/navigatorArguments.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
 class SignUp extends StatefulWidget {
   @override
   _SignUpState createState() => _SignUpState();
@@ -33,119 +32,136 @@ class _SignUpState extends State<SignUp> {
         ),
         body: Padding(
           padding: const EdgeInsets.all(10),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: usernameController,
-                  decoration: InputDecoration(hintText: "Username"),
-                  validator: (value) {
-                    if (value.isEmpty) return "Username cannot be empty";
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: emailController,
-                  decoration: InputDecoration(hintText: "Email"),
-                  validator: (value) {
-                    if (value.isEmpty) return "Email cannot be empty";
-
-                    bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(value);
-                    if (!emailValid) return "Email not correct format";
-
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    hintText: "Password",
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        Icons.lock,
-                        color: Theme.of(context).primaryColorDark
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          passwordVisible = !passwordVisible;
-                        });
-                      },
-                    )
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) return "Password cannot be empty";
-                    return null;
-                  },
-                  obscureText: passwordVisible,
-                ),
-                TextFormField(
-                  controller: confirmPasswordController,
-                  decoration: InputDecoration(
-                    hintText: "Confirm Password",
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        Icons.lock,
-                        color: Theme.of(context).primaryColorDark,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          confirmPasswordVisible = !confirmPasswordVisible;
-                        });
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.6,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Register New Account",
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.center),
+                    SizedBox(height: 30),
+                    TextFormField(
+                      controller: usernameController,
+                      decoration: InputDecoration(hintText: "Username"),
+                      validator: (value) {
+                        if (value.isEmpty) return "Username cannot be empty";
+                        return null;
                       },
                     ),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) return "Confirm Password cannot be empty";
-                    if (passwordController.text != confirmPasswordController.text) return "Passwords need to match";
-                    return null;
-                  },
-                  obscureText: confirmPasswordVisible,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Visibility(
-                    child: Text(
-                      "Username already exists",
-                      style: TextStyle(color: Colors.red)
-                    ),
-                    visible: usernameConflict,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Visibility(
-                    child: Text(
-                        "There is already an account associated with this email",
-                        style: TextStyle(color: Colors.red)
-                    ),
-                    visible: emailConflict,
-                  ),
-                ),
-                FlatButton(child: Text("Sign Up"), onPressed: () async {
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(hintText: "Email"),
+                      validator: (value) {
+                        if (value.isEmpty) return "Email cannot be empty";
 
-                  if (_formKey.currentState.validate()) {
-                    String result = await signUp(args.url, usernameController.text, passwordController.text, emailController.text);
+                        bool emailValid = RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(value);
+                        if (!emailValid) return "Email not correct format";
 
-                    if (result == "success") {
-                      usernameConflict = false;
-                      emailConflict = false;
-                      Fluttertoast.showToast(msg: "Successfully created account");
-                      Navigator.pop(context);
-                    } else if (result == "usernameConflict") {
-                      setState(() {
-                        usernameConflict = true;
-                      });
-                    } else if (result == "emailConflict") {
-                      setState(() {
-                        emailConflict = true;
-                      });
-                    } else if (result == "SocketException") {
-                      Fluttertoast.showToast(msg: "Error connecting to server");
-                    }
-                  }
-                })
-              ],
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                          hintText: "Password",
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.lock,
+                                color: Theme.of(context).primaryColorDark),
+                            onPressed: () {
+                              setState(() {
+                                passwordVisible = !passwordVisible;
+                              });
+                            },
+                          )),
+                      validator: (value) {
+                        if (value.isEmpty) return "Password cannot be empty";
+                        return null;
+                      },
+                      obscureText: passwordVisible,
+                    ),
+                    TextFormField(
+                      controller: confirmPasswordController,
+                      decoration: InputDecoration(
+                        hintText: "Confirm Password",
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            Icons.lock,
+                            color: Theme.of(context).primaryColorDark,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              confirmPasswordVisible = !confirmPasswordVisible;
+                            });
+                          },
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value.isEmpty)
+                          return "Confirm Password cannot be empty";
+                        if (passwordController.text !=
+                            confirmPasswordController.text)
+                          return "Passwords need to match";
+                        return null;
+                      },
+                      obscureText: confirmPasswordVisible,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Visibility(
+                        child: Text("Username already exists",
+                            style: TextStyle(color: Colors.red)),
+                        visible: usernameConflict,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Visibility(
+                        child: Text(
+                            "There is already an account associated with this email",
+                            style: TextStyle(color: Colors.red)),
+                        visible: emailConflict,
+                      ),
+                    ),
+                    FlatButton(
+                        child: Text("Sign Up", style: TextStyle(color: Colors.white)),
+                        color: Theme.of(context).primaryColor,
+                        onPressed: () async {
+                          if (_formKey.currentState.validate()) {
+                            String result = await signUp(
+                                args.url,
+                                usernameController.text,
+                                passwordController.text,
+                                emailController.text);
+
+                            if (result == "success") {
+                              usernameConflict = false;
+                              emailConflict = false;
+                              Fluttertoast.showToast(
+                                  msg: "Successfully created account");
+                              Navigator.pop(context);
+                            } else if (result == "usernameConflict") {
+                              setState(() {
+                                usernameConflict = true;
+                              });
+                            } else if (result == "emailConflict") {
+                              setState(() {
+                                emailConflict = true;
+                              });
+                            } else if (result == "SocketException") {
+                              Fluttertoast.showToast(
+                                  msg: "Error connecting to server");
+                            }
+                          }
+                        })
+                  ],
+                ),
+              ),
             ),
           ),
         ),
@@ -153,4 +169,3 @@ class _SignUpState extends State<SignUp> {
     );
   }
 }
-
